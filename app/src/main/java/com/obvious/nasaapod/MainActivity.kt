8665302731b9e477fa36c358fbf7ab5f0a7ec267
Paras Doshi <pdoshi@roku.com>
2022-02-24 12:33:37 -0800

    Navigate to home fragment from Main

--- app/src/main/java/com/obvious/nasaapod/MainActivity.kt
+++ app/src/main/java/com/obvious/nasaapod/MainActivity.kt
@@ -2,10 +2,16 @@ package com.obvious.nasaapod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.obvious.nasaapod.home.view.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, HomeFragment(), HomeFragment::class.java.name)
+            .commit()
     }
 }
