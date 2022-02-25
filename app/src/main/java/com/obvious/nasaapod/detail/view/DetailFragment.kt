1e07fd5e9f198cfc94c31083487019607ade5eee
Paras Doshi <pdoshi@roku.com>
2022-02-25 03:17:11 -0800

    Change title per page

--- app/src/main/java/com/obvious/nasaapod/detail/view/DetailFragment.kt
+++ app/src/main/java/com/obvious/nasaapod/detail/view/DetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.obvious.nasaapod.databinding.FragmentImageDetailBinding
 import com.obvious.nasaapod.home.data.ImageDto
 import com.xwray.groupie.GroupAdapter
@@ -14,8 +15,16 @@ class DetailFragment : Fragment() {
 
     private var _detailLayoutBinding: FragmentImageDetailBinding? = null
     private val detailLayoutBinding get() = _detailLayoutBinding!!
+    private var imageList: List<ImageDto> = listOf()
 
     private val groupAdapter = GroupAdapter<GroupieViewHolder>()
+
+    private val onPageChangeListener = object : OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            detailLayoutBinding.toolbar.title = imageList[position].title ?: ""
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,30 +38,40 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val imageList = arguments?.getParcelableArrayList<ImageDto>("s")
-        val selectedPosition = arguments?.getInt("i") ?: 0
+        imageList = arguments?.getParcelableArrayList(BUNDLE_IMAGE_LIST) ?: listOf()
+        val selectedPosition = arguments?.getInt(BUNDLE_POSITION) ?: 0
 
-        imageList?.forEach {
+        imageList.forEach {
             groupAdapter.add(ImageDetailItem(it, requireContext()))
         }
 
         detailLayoutBinding.viewpager.apply {
             adapter = groupAdapter
             offscreenPageLimit = 2
+            registerOnPageChangeCallback(onPageChangeListener)
         }
 
         detailLayoutBinding.viewpager.setCurrentItem(selectedPosition, false)
+    }
+
+    private fun setupToolbar() {
+        detailLayoutBinding.toolbar.apply {
+            //navigationIcon =
+        }
     }
 
     companion object {
         @JvmStatic
         fun newInstance(imageList: List<ImageDto>, selectedPosition: Int): DetailFragment {
             val bundle = Bundle()
-            bundle.putParcelableArrayList("s", ArrayList(imageList))
-            bundle.putInt("i", selectedPosition)
+            bundle.putParcelableArrayList(BUNDLE_IMAGE_LIST, ArrayList(imageList))
+            bundle.putInt(BUNDLE_POSITION, selectedPosition)
             val fragment = DetailFragment()
             fragment.arguments = bundle
             return fragment
         }
+
+        const val BUNDLE_IMAGE_LIST = "image_list"
+        const val BUNDLE_POSITION = "position"
     }
 }
